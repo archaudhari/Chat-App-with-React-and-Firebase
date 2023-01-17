f0ceb69bbd5e6b6c15fdc502bd1e67072e0349b2
archaudhari <81674825+archaudhari@users.noreply.github.com>
2023-01-17 22:10:36 +0530

    1 file modified

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
-import GoogleSignin from '../img/btn_google_signin_dark_pressed_web.png';
+import GoogleSignin from '../img/GoogleSignUpDark.png';
+import { auth } from '../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
 
 const NavBar = () => {
-  const [user, setUser] = useState(false);
+  const [user] = useAuthState(auth);
   const googleSignIn = () => {
-    setUser(true);
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
   };
   const signOut = () => {
-    setUser(false);
+    auth.signOut();
   };
   return (
     <nav className="nav-bar">
